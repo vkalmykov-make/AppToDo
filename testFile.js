fd3b500bfe82921dac87aa777ef4e94a7c60897c
Valentin Kalmykov <v.kalmykov@make.com>
2022-11-09 21:58:07 +0100

    first commit

---
+++ testFile.js
@@ -0,0 +1,7 @@
+const todoData = new Map([["1", { name: "jhello ", autor: "some", id: "1", task: "DoImportantThing" }],["2", { name: "asd2 ", autor: "some2", id: "2", task: "SomeTask" }]]);
+  //cos
+const todoDataList = Object.entries(todoData)
+  let todoItem = undefined 
+  todoData.set('2',{ name: `${name}`, autor: {}, id: "3", task: "afasfa" })
+  console.log(todoData.get('2'))
+  
